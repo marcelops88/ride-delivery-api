963536618e0b44d4cc2e615d3766f35eee549573
Marcelo Pereira de Santana <marcelo.santana@castgroup.com.br>
2024-10-15 13:39:21 -0300

    feat: adicionar configuração do Swagger para documentação da API feat: configurar Serilog para log em formato JSON feat: adicionar suporte a CORS com política de permitir todas as origens feat: configurar serviços iniciais da aplicação feat: adicionar middleware e configuração para ambientes de desenvolvimento e produção

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,25 +1,115 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Interfaces;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Formatting.Json;
+using System.Reflection;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace API
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    internal class Program
+    {
+        private static IConfiguration Configuration { get; } = BuildConfiguration();
+
+        private static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            ConfigureSwagger(builder);
+            ConfigureLogging();
+            ConfigureServices(builder);
+            AddServices(builder);
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder => builder.AllowAnyOrigin()
+                                     .AllowAnyMethod()
+                                     .AllowAnyHeader());
+            });
+            builder.Host.UseSerilog();
+
+            var app = builder.Build();
+
+            ConfigureMiddleware(app);
+
+            app.Run();
+        }
+
+        private static void AddServices(WebApplicationBuilder builder)
+        {
+
+        }
+
+        private static IConfiguration BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? string.Empty}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        private static async void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+        }
+
+        private static void ConfigureSwagger(WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "RideAndDeliver API",
+                    Version = "v1",
+                    Description = "A RideAndDeliver API é responsável por gerenciar operações de locação de motos e entregadores, como cadastro, consulta, alteração e devolução de motos. Ela também permite o envio de CNH e o gerenciamento de locações ativas.",
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "Equipe RideAndDeliver",
+                        Email = "suporte@rideanddeliver.com.br",
+                    },
+                        Extensions = new Dictionary<string, IOpenApiExtension>
+                {
+                    {
+                        "x-logo", new OpenApiObject
+                        {
+                            { "url", new OpenApiString("https://motociclismoonline.com.br/wp-content/uploads/2024/05/aluguel_de_motos_pixabay.jpg") },
+                            { "altText", new OpenApiString("RideAndDeliver API") }
+                        }
+                    }
+                }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                config.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        private static void ConfigureLogging()
+        {
+            var minLogLevel = Serilog.Events.LogEventLevel.Information;
+
+            var loggerConfiguration = new LoggerConfiguration()
+                .MinimumLevel.Is(minLogLevel)
+                .WriteTo.Console(formatter: new JsonFormatter())
+                .WriteTo.File("logs/ride-delivery-api.json", rollingInterval: RollingInterval.Day);
+            Log.Logger = loggerConfiguration.CreateLogger();
+        }
+
+        private static void ConfigureMiddleware(WebApplication app)
+        {
+            if (!app.Environment.IsProduction())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("AllowAllOrigins");
+            app.MapControllers();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
